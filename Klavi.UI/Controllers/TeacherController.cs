4ffa43acbb13c5178eca4b65bdc956fbc42c7b33
leylam22 <leylarm@code.edu.az>
2023-09-22 02:11:32 +0400

    Teacher page added

---
+++ Klavi.UI/Controllers/TeacherController.cs
@@ -0,0 +1,26 @@
+﻿using Kalvi.DataAccess.Contexts;
+using Klavi.UI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Klavi.UI.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TeacherController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            TeacherVm teacherVm = new()
+            {
+                Teachers= await _context.Teachers.ToListAsync(),
+            };
+            return View(teacherVm);
+        }
+    }
+}
---
+++ Klavi.UI/ViewModel/TeacherVm.cs
@@ -0,0 +1,9 @@
+﻿using Kalvi.Core.Entities;
+
+namespace Klavi.UI.ViewModel
+{
+    public class TeacherVm
+    {
+        public IEnumerable<Teachers> Teachers { get; set; }
+    }
+}
