ecd4a220b9c861463f2ea0211f5ae6843ada2cd3
leylam22 <leylarm@code.edu.az>
2023-09-23 23:11:13 +0400

    Course and course detail is added

---
+++ Klavi.UI/Controllers/CourseController.cs
@@ -0,0 +1,43 @@
+﻿using Kalvi.DataAccess.Contexts;
+using Klavi.UI.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Klavi.UI.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CourseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            HomeVM vm = new HomeVM()
+            {
+                Courses = await _context.Courses.ToListAsync(),
+                CourseCategories = await _context.CourseCategories.ToListAsync(),
+                CourseDetails = await _context.CourseDetails.ToListAsync()
+            };
+            return View(vm);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            if (id == 0) return NotFound();
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null) return NotFound();
+            ViewBag.CourseId = course.Id;
+            HomeVM vm = new()
+            {
+                Courses = await _context.Courses.ToListAsync(),
+                CourseCategories = await _context.CourseCategories.ToListAsync(),
+                CourseDetails = await _context.CourseDetails.ToListAsync()
+            };
+            return View(vm);
+        }
+    }
+}
--- Klavi.UI/ViewModel/HomeVM.cs
+++ Klavi.UI/ViewModel/HomeVM.cs
@@ -6,6 +6,7 @@ public class HomeVM
 {
     public IEnumerable<Course> Courses { get; set; }
     public IEnumerable<CourseCategory> CourseCategories { get; set; }
+    public IEnumerable<CourseDetail> CourseDetails { get; set; }
     public IEnumerable<Teachers> Teachers { get; set; }
     public IEnumerable<Blog> Blogs { get; set; }
 }
